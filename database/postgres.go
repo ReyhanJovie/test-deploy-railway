ed1306b7e224ab256459b390e443cbbfe50c2417
Reyhan Jovie Dwiputra <reyhanjovie01@gmail.com>
2023-12-14 20:31:38 +0700

    fix dsn

--- database/postgres.go
+++ database/postgres.go
@@ -8,7 +8,7 @@ import (
 )
 
 func ConnectDB(host, port, user, password, name string) (db *sql.DB, err error) {
-	dsn := fmt.Sprintf("host=%v port=%v user=%v password=%v name=%v sslmode=disable", host, port, user, password, name)
+	dsn := fmt.Sprintf("host=%v port=%v user=%v password=%v dbname=%v sslmode=disable", host, port, user, password, name)
 
 	db, err = sql.Open("postgres", dsn)
 	if err != nil {
